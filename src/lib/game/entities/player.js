ee30f256f866aa02d669759cc475fef1402c85aa
Justin Stahlman <jstahlman@mac.com>
2015-04-05 00:02:35 -0400

    Initial commit

---
+++ src/lib/game/entities/player.js
@@ -0,0 +1,55 @@
+ig.module(
+    'game.entities.player'
+)
+.requires(
+    'impact.entity'
+)
+.defines(function() {
+
+    EntityPlayer = ig.Entity.extend({
+
+        size: { x: 24, y: 24 },
+		maxVel: {x: 100, y: 100},
+		friction: {x: 100, y: 100},
+		step: {x:640,y:640},
+		speed: 100,
+
+        collides: ig.Entity.COLLIDES.PASSIVE,
+
+        animSheet: new ig.AnimationSheet('media/player.png', 24, 24),
+
+        init: function (x, y, settings) {
+
+			ig.game.player = this;
+
+            this.parent(x, y, settings);
+
+            this.addAnim('init', 1, [0]);
+        },
+
+        update: function() {
+
+            this.parent();
+
+        	if (ig.input.pressed('left')) {
+				//this.vel.x -= this.step.x;
+				this.pos.x -= 320;
+			}
+			else
+			if (ig.input.pressed('right')) {
+				//this.vel.x += this.step.x;
+				this.pos.x += 320;
+			}
+			else
+			if (ig.input.pressed('up')) {
+				//this.vel.y -= this.step.y;
+				this.pos.y -= 512;
+			}
+			else
+			if (ig.input.pressed('down')) {
+				//this.vel.y += this.step.y
+				this.pos.y += 512;
+			}
+        }
+    });
+});
---
+++ src/lib/game/main.js
@@ -0,0 +1,75 @@
+ig.module(
+	'game.main'
+)
+.requires(
+	'impact.game',
+	'impact.font',
+	'plugins.camera',				// UI components
+	'game.levels.dungeon1'
+)
+.defines(function(){
+
+MyGame = ig.Game.extend({
+
+	// Load a font
+	font: new ig.Font( 'media/04b03.font.png' ),
+	player: {},
+
+	init: function() {
+		// Initialize your game here; bind keys etc.
+		ig.input.bind(ig.KEY.LEFT_ARROW, 'left');
+		ig.input.bind(ig.KEY.RIGHT_ARROW, 'right');
+		ig.input.bind(ig.KEY.UP_ARROW, 'up');
+		ig.input.bind(ig.KEY.DOWN_ARROW, 'down');
+
+		// Set up the camera. The camera's center is at a third of the screen
+		// size, i.e. somewhat shift left and up. Damping is set to 3px.
+		this.camera = new ig.Camera( ig.system.width/2, ig.system.height/2, 5 );
+
+		// The camera's trap (the deadzone in which the player can move with the
+		// camera staying fixed) is set to according to the screen size as well.
+    	//this.camera.trap.size.x = ig.system.width/10;
+    	//this.camera.trap.size.y = ig.system.height/3;
+
+		// The lookahead always shifts the camera in walking position; you can
+		// set it to 0 to disable.
+    	//this.camera.lookAhead.x = ig.system.width/6;
+
+		this.loadLevel(ig.global['Level' + 'Dungeon1']);
+	},
+
+	update: function() {
+		// Update all entities and backgroundMaps
+		this.parent();
+		this.camera.follow(this.player);
+
+
+	},
+	loadLevel: function(data){
+
+		this.parent(data);
+		// Set camera's screen bounds and reposition the trap on the player
+    	this.camera.max.x = this.collisionMap.pxWidth - ig.system.width;
+    	this.camera.max.y = this.collisionMap.pxHeight - ig.system.height;
+    	this.camera.set( this.player );
+	},
+
+	draw: function() {
+		// Draw all entities and backgroundMaps
+		this.parent();
+
+
+		// Add your own drawing code here
+		var x = ig.system.width/2,
+			y = ig.system.height/2;
+
+		this.font.draw( 'It Works!', x, y, ig.Font.ALIGN.CENTER );
+	}
+});
+
+
+// Start the Game with 60fps, a resolution of 320x240, scaled
+// up by a factor of 2
+ig.main( '#canvas', MyGame, 60, 320, 512, 1 );
+
+});
